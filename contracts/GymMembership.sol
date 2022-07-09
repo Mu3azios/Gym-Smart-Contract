540718197c37023324ce90fdd94f0ba85e1749ae
Muaz Baytamouny <mu3az.92@gmail.com>
2022-07-09 15:46:10 +0800

    function transfer

--- contracts/GymMembership.sol
+++ contracts/GymMembership.sol
@@ -12,16 +12,19 @@ contract GymMembership {
   uint membershipFees= 0.1;
   mapping(address => string) public Members; //mapping to see the owner of address
   mapping(address => bool) public paid;
+  modifier _isRegistered(){
+    require(members[msg.sender]== paid, "this user is registered");
+    _;
+  }
 function register(string memory name) public{
 name[msg.sender] = name;
-members.push(Member(_name));
-
+members.push(Member(string, name));
 }
-  modifier _isRegistered(bool memory _name){
-    require(members[_name]==registered, "this user is registered");
-  }
-  function pay(amount) public{
-    require(paid[msg.sender]==false);
+  // function pay(uint amount) public{
+  //   require(paid[msg.sender]==false, "this user has paid
+
+  function transfer(address _to , uint256 _value) public returns (bool paid){
 
   }
-}
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
